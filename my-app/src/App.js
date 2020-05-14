f7bf40dc14a62b3cdcec649fe680f122d0518d87
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-05-14 15:53:01 -0400

    Adding h2 and p elements, adding inputChange, validate, etc

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,9 @@ function App() {
       <div className="header">
         <h1>💃🍔The 5th Annual Bob's Burgers Ball!🍔🤵‍</h1>
         <img src="https://66.media.tumblr.com/ea714643037906db0da2324374c6017d/tumblr_ohdffpMZGT1udklowo1_540.gifv"></img>
+        <p>When: Saturday, December 19, 2020</p>
+        <p>Where: Bob's Burgers, </p>
+
         <h2>RSVP Below!</h2>
       </div>
       <Form />
--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as yup from "yup";
 import axios from "axios";
 
@@ -9,22 +9,55 @@ function Form() {
         secretPassword: "",
         totalGuests: "",
         favoriteCharacter: "",
-        allergies: false
+        foodAllergies: false
 
     });
 
+    const [errorState, setErrorState] = useState({
+        name: "",
+        email: "",
+        secretPassword: "",
+        totalGuests: "",
+        favoriteCharacter: "",
+        foodAllergies: ""
+    })
+
+    const validate = (e) => {
+        yup 
+            .reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid => {
+                setErrorState({
+                    ...errorState, [e.target.name]: ""
+                });
+            })
+            .catch(err => {
+                console.log(err.errors);
+                setErrorState({
+                    ...errorState,
+                    [e.target.name]: err.errors[0]
+                })
+            });
+    };
+
     const inputChange = e => {
         e.persist();
-    }
+        validate(e);
+            let value =
+            e.target.type === "checkbox" ?
+                e.target.checked : 
+                e.target.value;
+            setFormState({ ...formState, [e.target.name]: value});
+    };
 
-    const formSubmit = () => {
+    const formSubmit = (e) => {
         e.preventDefault();
         console.log("form submitted!");
         axios 
             .post("https://reqres.in/api/users", formState)
             .then(response => console.log(response))
             .catch(err => console.log(err));
-    }
+    };
     return (
         <form onSubmit={formSubmit}>
             <label htmlFor="name">
